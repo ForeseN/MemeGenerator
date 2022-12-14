bca71e16b7693b7f846de5800c0230e3e7deb245
ForeseN <yagosik@gmail.com>
2022-12-14 11:16:45 +0200

    added basic meme generator

--- js/memeController.js
+++ js/memeController.js
@@ -6,5 +6,61 @@ let gCtx
 function onInit() {
     gElCanvas = document.getElementById('my-canvas')
     gCtx = gElCanvas.getContext('2d')
+    defaultConfig()
     // resizeCanvas()
+    const meme = getMeme(3)
+    console.log(meme)
+    renderMeme(meme)
+}
+
+function defaultConfig() {
+    gCtx.font = '40px impact'
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+    gCtx.lineWidth = 2
+}
+
+function renderMeme(meme) {
+    let img = new Image() // Create a new html img element
+    img.src = meme.url // Set the img src
+    img.onload = () => {
+        renderImg(img)
+        meme.lines.forEach(line => {
+            const { txt, size, font, align, fillColor, strokeColor } = line
+            drawText(
+                txt,
+                gElCanvas.width / 2,
+                50,
+                size,
+                font,
+                align,
+                fillColor,
+                strokeColor
+            )
+        })
+    }
+}
+
+// ---------------------- ON FUNCTIONS  ----------------------
+
+
+
+// ---------------------- DRAW & RENDER ----------------------
+
+function drawText(text, x, y, size, font, align, fillColor, strokeColor) {
+    gCtx.font = `${size}px ${font}`
+    console.log(fillColor)
+    gCtx.fillStyle = fillColor
+    gCtx.strokeStyle = strokeColor
+    gCtx.textAlign = align
+    gCtx.fillText(text, x, y) // Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(text, x, y) // Draws (strokes) a given text at the given (x, y) position.
+    defaultConfig() // revert settings
+}
+
+function renderImg(img) {
+    // Draw the img on the canvas
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
 }
---
+++ js/services/meme-service.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var gKeywordSearchCountMap = { funny: 12, cat: 16, baby: 2 }
+var gImgs = [
+    { id: 1, url: '../../meme-imgs-square/1.jpg', keywords: ['funny', 'dog'] },
+    { id: 2, url: '../../meme-imgs-square/2.jpg', keywords: ['funny', 'dog'] },
+    { id: 3, url: '../../meme-imgs-square/3.jpg', keywords: ['funny', 'what'] },
+    { id: 4, url: '../../meme-imgs-square/4.jpg', keywords: ['funny', 'dog'] },
+]
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    url: '../../meme-imgs-square/1.jpg',
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 40,
+            font: 'impact',
+            align: 'center',
+            fillColor: 'white',
+            strokeColor: 'black',
+        },
+    ],
+}
+
+function getMeme(id) {
+    const img = gImgs.find(img => img.id === id)
+    gMeme.selectedImgId = id
+    gMeme.url = img.url
+    return gMeme
+}
