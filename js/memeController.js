c48383a52982c0cc53f320490f04c6ffc9792db6
ForeseN <yagosik@gmail.com>
2022-12-14 10:33:28 +0200

    git init

---
+++ js/memeController.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.getElementById('my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    // resizeCanvas()
+}
